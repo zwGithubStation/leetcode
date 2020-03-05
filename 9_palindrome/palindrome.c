098eadfc5612139b71700129e856f0ec14067595
zwGithubStation <zwp_sz_pro@163.com>
2020-03-05 12:34:38 +0800

    palindrome skeleton

---
+++ 9_palindrome/palindrome.c
@@ -0,0 +1,26 @@
+
+/*
+ * Copyright (C) ZWP
+
+ * Problem: https://leetcode-cn.com/problems/palindrome-number/
+
+   solve  : 
+
+   compile: gcc -std=c99 -fsanitize=address -fno-omit-frame-pointer -O1 -g palindrome.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+bool isPalindrome(int x){
+	return true;
+}
+
+
+int main()
+{
+	return 0;
+}
+
