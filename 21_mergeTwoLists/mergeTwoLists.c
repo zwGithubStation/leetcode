3fdab6f50eb4f0e2ea579b818c138f6c036bcdfe
zwGithubStation <zwp_sz_pro@163.com>
2020-03-06 17:27:59 +0800

    merge list complete

--- 21_mergeTwoLists/mergeTwoLists.c
+++ 21_mergeTwoLists/mergeTwoLists.c
@@ -4,7 +4,11 @@
 
  * Problem: https://leetcode-cn.com/problems/merge-two-sorted-lists/
 
-   solve  : 
+   solve  : 1.self-solve(two lists, cur always towards to lower list's current head, 
+   						 comp always towards to higher list's current head, 
+   						 usd pre/post pointers facilitate the cur/comp change,
+   						 make sure when cur is searched over, comp's tail could be included)
+			2.official(iteration)
 
    compile: gcc -std=c99 -fsanitize=address -fno-omit-frame-pointer -O1 -g mergeTwoLists.c
  */
@@ -25,7 +29,8 @@ struct ListNode {
    struct ListNode *next;
  };
 
-
+//self-solve
+/*
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
 	struct ListNode* result = NULL;
 	struct ListNode* cur, *comp, *pre, *post;
@@ -58,9 +63,52 @@ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
 			comp = post;
 		}
 	}
+
+	if (comp != NULL)
+	{
+		pre->next = comp;
+	}
 	
 	return result;
 	
+}
+*/
+
+//official
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
+	struct ListNode* result = (ListNode *)malloc(sizeof(struct ListNode));
+	struct ListNode* cur = result;
+		
+	result->next = NULL;
+	while (l1 != NULL || l2 != NULL)
+	{
+		if (l1 == NULL)
+		{
+			cur->next = l2;
+			return result->next;
+		}
+
+		if (l2 == NULL)
+		{
+			cur->next = l1;
+			return result->next;
+		}
+
+		if (l1->val <= l2->val)
+		{
+			cur->next = l1;
+			l1 = l1->next;
+		}
+		else
+		{
+			cur->next = l2;
+			l2 = l2->next;
+		}
+
+		cur = cur->next;
+	}
+
+	return result->next;
 }
 
 
