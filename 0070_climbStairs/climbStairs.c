0d8898d38a777290c5e684e5d4075f32eb06618d
zwGithubStation <zwp_sz_pro@163.com>
2020-03-13 16:03:01 +0800

    climb baseline debug

--- 0070_climbStairs/climbStairs.c
+++ 0070_climbStairs/climbStairs.c
@@ -22,6 +22,7 @@ int combine(int x, int y)
 	long long y1 = (long long)y;
 	long long divide = 1;
 	long long divide_by = 1;
+	long long ret;
 	
 	if (y1 == 0 || x1 == y1)
 	{
@@ -35,8 +36,9 @@ int combine(int x, int y)
 		divide_by *= y1--;
 	}
 
-	printf("C(%d,%d) = (%lld / %lld) = %d\n",x, y, divide, divide_by, (int)divide/divide_by);
-	return (int)divide/divide_by;
+	printf("C(%d,%d) = (%lld / %lld) = %lld\n",x, y, divide, divide_by, divide/divide_by);
+	ret = divide/divide_by;
+	return (int)ret;
 	
 }
 
