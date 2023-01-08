2030836c15b235ab32ce37868259e1cffa5da1c4
zwGithubStation <zwp_sz_pro@163.com>
2023-01-08 21:45:26 +0800

    327 1

--- weekCamp/327/code/code.go
+++ weekCamp/327/code/code.go
@@ -1,7 +1,35 @@
 package code
 
+import "sort"
+
 func maximumCount(nums []int) int {
-	return 1
+	negCnt, posCnt := 0, 0
+	for _, x := range nums {
+		if x < 0 {
+			negCnt++
+		}
+		if x > 0 {
+			posCnt++
+		}
+	}
+	if posCnt >= negCnt {
+		return posCnt
+	}
+	return negCnt
+}
+
+//使用二分查找优化上面函数
+//熟练使用下标
+func max(x int, y int) int {
+	if x >= y {
+		return x
+	}
+	return y
+}
+func maximumCountBySearch(nums []int) int {
+	negCnt := sort.SearchInts(nums, 0)
+	posCnt := len(nums) - sort.SearchInts(nums, 1)
+	return max(negCnt, posCnt)
 }
 
 func maxKelements(nums []int, k int) int64 {
