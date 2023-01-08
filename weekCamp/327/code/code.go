a02ecf8225206ef8a345c9c6c6255bc6d57bd5f0
zwGithubStation <zwp_sz_pro@163.com>
2023-01-08 21:26:13 +0800

    327 skele

--- weekCamp/327/code/code.go
+++ weekCamp/327/code/code.go
@@ -1,1 +1,17 @@
 package code
+
+func maximumCount(nums []int) int {
+	return 1
+}
+
+func maxKelements(nums []int, k int) int64 {
+	return 1
+}
+
+func isItPossible(word1 string, word2 string) bool {
+	return true
+}
+
+func findCrossingTime(n int, k int, time [][]int) int {
+	return 1
+}
--- weekCamp/327/code/code_test.go
+++ weekCamp/327/code/code_test.go
@@ -1,1 +1,87 @@
 package code
+
+import "testing"
+
+func Test_maximumCount(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maximumCount(tt.args.nums); got != tt.want {
+				t.Errorf("maximumCount() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_maxKelements(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxKelements(tt.args.nums, tt.args.k); got != tt.want {
+				t.Errorf("maxKelements() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isItPossible(t *testing.T) {
+	type args struct {
+		word1 string
+		word2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isItPossible(tt.args.word1, tt.args.word2); got != tt.want {
+				t.Errorf("isItPossible() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_findCrossingTime(t *testing.T) {
+	type args struct {
+		n    int
+		k    int
+		time [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findCrossingTime(tt.args.n, tt.args.k, tt.args.time); got != tt.want {
+				t.Errorf("findCrossingTime() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
