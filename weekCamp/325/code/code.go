fe5e2d4715d068246eaeb838cb095bfa27ccc054
zwGithubStation <zwp_sz_pro@163.com>
2023-01-08 15:34:36 +0800

    325 1

--- weekCamp/325/code/code.go
+++ weekCamp/325/code/code.go
@@ -1,1 +1,33 @@
 package code
+
+//给你一个下标从 0 开始的 环形 字符串数组 words 和一个字符串 target 。环形数组 意味着数组首尾相连。
+//
+//形式上， words[i] 的下一个元素是 words[(i + 1) % n] ，而 words[i] 的前一个元素是 words[(i - 1 + n) % n] ，其中 n 是 words 的长度。
+//从 startIndex 开始，你一次可以用 1 步移动到下一个或者前一个单词。
+//返回到达目标字符串 target 所需的最短距离。如果 words 中不存在字符串 target ，返回 -1 。
+func closetTarget(words []string, target string, startIndex int) int {
+	var length int = len(words)
+	if words[startIndex] == target {
+		return 0
+	}
+	for i := 1; i <= length/2; i++ {
+		if words[(startIndex+i)%length] == target {
+			return i
+		}
+		if words[(startIndex-i+length)%length] == target {
+			return i
+		}
+	}
+	return -1
+}
+func takeCharacters(s string, k int) int {
+	return 1
+}
+
+func maximumTastiness(price []int, k int) int {
+	return 1
+}
+
+func countPartitions(nums []int, k int) int {
+	return 1
+}
--- weekCamp/325/code/code_test.go
+++ weekCamp/325/code/code_test.go
@@ -1,1 +1,94 @@
 package code
+
+import "testing"
+
+func Test_closetTarget(t *testing.T) {
+	type args struct {
+		words      []string
+		target     string
+		startIndex int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"test1", args{[]string{"hello", "i", "am", "leetcode", "hello"}, "hello", 1}, 1},
+		{"test2", args{[]string{"a", "b", "leetcode"}, "leetcode", 0}, 1},
+		{"test3", args{[]string{"i", "eat", "leetcode"}, "ate", 0}, -1},
+		{"test4", args{[]string{"pgmiltbptl", "jnkxwutznb", "bmeirwjars",
+			"ugzyaufzzp", "pgmiltbptl", "sfhtxkmzwn",
+			"pgmiltbptl", "pgmiltbptl", "onvmgvjhxa", "jyzdtwbwqp"}, "pgmiltbptl", 4}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := closetTarget(tt.args.words, tt.args.target, tt.args.startIndex); got != tt.want {
+				t.Errorf("closetTarget() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_takeCharacters(t *testing.T) {
+	type args struct {
+		s string
+		k int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := takeCharacters(tt.args.s, tt.args.k); got != tt.want {
+				t.Errorf("takeCharacters() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_maximumTastiness(t *testing.T) {
+	type args struct {
+		price []int
+		k     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maximumTastiness(tt.args.price, tt.args.k); got != tt.want {
+				t.Errorf("maximumTastiness() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_countPartitions(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countPartitions(tt.args.nums, tt.args.k); got != tt.want {
+				t.Errorf("countPartitions() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
