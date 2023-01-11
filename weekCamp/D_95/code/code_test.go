3a3b43dc4ca94bf7f7a2bf9d4fd2e87e3ea39b92
zwGithubStation <zwp_sz_pro@163.com>
2023-01-11 19:27:05 +0800

    95 4 test

--- weekCamp/D_95/code/code_test.go
+++ weekCamp/D_95/code/code_test.go
@@ -65,3 +65,25 @@ func Test_xorBeauty(t *testing.T) {
 		})
 	}
 }
+
+func Test_maxPower(t *testing.T) {
+	type args struct {
+		stations []int
+		r        int
+		k        int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxPower(tt.args.stations, tt.args.r, tt.args.k); got != tt.want {
+				t.Errorf("maxPower() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
