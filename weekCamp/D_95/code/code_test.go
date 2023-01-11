e1e1ee265fc6b43de2250fa4a4f5bc452c7785f6
zwGithubStation <zwp_sz_pro@163.com>
2023-01-11 19:26:07 +0800

    95 1 test

--- weekCamp/D_95/code/code_test.go
+++ weekCamp/D_95/code/code_test.go
@@ -1,1 +1,26 @@
 package code
+
+import "testing"
+
+func Test_categorizeBox(t *testing.T) {
+	type args struct {
+		length int
+		width  int
+		height int
+		mass   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := categorizeBox(tt.args.length, tt.args.width, tt.args.height, tt.args.mass); got != tt.want {
+				t.Errorf("categorizeBox() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
