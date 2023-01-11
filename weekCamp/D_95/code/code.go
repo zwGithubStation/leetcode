f6f0c7cb30abf79cb824ada2d10dc8038458acc0
zwGithubStation <zwp_sz_pro@163.com>
2023-01-11 19:25:09 +0800

    95 1

--- weekCamp/D_95/code/code.go
+++ weekCamp/D_95/code/code.go
@@ -3,3 +3,20 @@ package code
 func categorizeBox(length int, width int, height int, mass int) string {
 	return ""
 }
+
+type DataStream struct {
+}
+
+func Constructor(value int, k int) DataStream {
+
+}
+
+func (this *DataStream) Consec(num int) bool {
+	return true
+}
+
+/**
+ * Your DataStream object will be instantiated and called as such:
+ * obj := Constructor(value, k);
+ * param_1 := obj.Consec(num);
+ */
