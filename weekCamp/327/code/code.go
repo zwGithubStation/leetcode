f1767c055ea7be4d7e11a67484d7abc8f4642cb5
zwGithubStation <zwp_sz_pro@163.com>
2023-01-16 22:24:15 +0800

    327 3

--- weekCamp/327/code/code.go
+++ weekCamp/327/code/code.go
@@ -52,8 +52,35 @@ func (h hp) Less(i, j int) bool { return h.IntSlice[i] > h.IntSlice[j] }
 func (hp) Push(interface{})     {}
 func (hp) Pop() (_ interface{}) { return }
 
-func isItPossible(word1 string, word2 string) bool {
-	return true
+func isItPossible(word1, word2 string) bool {
+	c1 := map[rune]int{}
+	for _, c := range word1 {
+		c1[c]++
+	}
+	c2 := map[rune]int{}
+	for _, c := range word2 {
+		c2[c]++
+	}
+	for x, c := range c1 {
+		for y, d := range c2 {
+			if y == x { // 无变化
+				if len(c1) == len(c2) {
+					return true
+				}
+			} else if len(c1)-b2i(c == 1)+b2i(c1[y] == 0) ==
+				len(c2)-b2i(d == 1)+b2i(c2[x] == 0) { // 基于长度计算变化量
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func b2i(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
 }
 
 func findCrossingTime(n int, k int, time [][]int) int {
