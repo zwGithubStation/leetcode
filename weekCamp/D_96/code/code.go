11993e4d017a2c64e332152c27547cf296ecc517
zwGithubStation <zwp_sz_pro@163.com>
2023-01-22 08:56:11 +0800

    D96

--- weekCamp/D_96/code/code.go
+++ weekCamp/D_96/code/code.go
@@ -16,6 +16,28 @@ func getCommon(nums1 []int, nums2 []int) int {
 	return 1
 }
 
+/*
+给你两个整数数组 nums1 和 nums2 ，两个数组长度都是 n ，再给你一个整数 k 。你可以对数组 nums1 进行以下操作：
+选择两个下标 i 和 j ，将 nums1[i] 增加 k ，将 nums1[j] 减少 k 。换言之，nums1[i] = nums1[i] + k 且 nums1[j] = nums1[j] - k 。
+如果对于所有满足 0 <= i < n 都有 num1[i] == nums2[i] ，那么我们称 nums1 等于 nums2 。
+请你返回使 nums1 等于 nums2 的 最少 操作数。如果没办法让它们相等，请你返回 -1 。
+示例 1：
+输入：nums1 = [4,3,1,4], nums2 = [1,3,7,1], k = 3
+输出：2
+解释：我们可以通过 2 个操作将 nums1 变成 nums2 。
+第 1 个操作：i = 2 ，j = 0 。操作后得到 nums1 = [1,3,4,4] 。
+第 2 个操作：i = 2 ，j = 3 。操作后得到 nums1 = [1,3,7,1] 。
+无法用更少操作使两个数组相等。
+示例 2：
+输入：nums1 = [3,8,5,2], nums2 = [2,4,1,6], k = 1
+输出：-1
+解释：无法使两个数组相等。
+提示：
+n == nums1.length == nums2.length
+2 <= n <= 105
+0 <= nums1[i], nums2[j] <= 109
+0 <= k <= 105
+*/
 func minOperations(nums1 []int, nums2 []int, k int) int64 {
 	return 1
 }
