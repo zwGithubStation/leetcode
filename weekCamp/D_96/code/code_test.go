4503fe9b404dd930af1e552344f39bd4b9e14951
zwGithubStation <zwp_sz_pro@163.com>
2023-01-22 09:00:10 +0800

    D96

--- weekCamp/D_96/code/code_test.go
+++ weekCamp/D_96/code/code_test.go
@@ -2,53 +2,10 @@ package code
 
 import "testing"
 
-func Test_categorizeBox(t *testing.T) {
+func Test_getCommon(t *testing.T) {
 	type args struct {
-		length int
-		width  int
-		height int
-		mass   int
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := categorizeBox(tt.args.length, tt.args.width, tt.args.height, tt.args.mass); got != tt.want {
-				t.Errorf("categorizeBox() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestDataStream_Consec(t *testing.T) {
-	type args struct {
-		num int
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			this := &DataStream{}
-			if got := this.Consec(tt.args.num); got != tt.want {
-				t.Errorf("Consec() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_xorBeauty(t *testing.T) {
-	type args struct {
-		nums []int
+		nums1 []int
+		nums2 []int
 	}
 	tests := []struct {
 		name string
@@ -59,18 +16,18 @@ func Test_xorBeauty(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := xorBeauty(tt.args.nums); got != tt.want {
-				t.Errorf("xorBeauty() = %v, want %v", got, tt.want)
+			if got := getCommon(tt.args.nums1, tt.args.nums2); got != tt.want {
+				t.Errorf("getCommon() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func Test_maxPower(t *testing.T) {
+func Test_minOperations(t *testing.T) {
 	type args struct {
-		stations []int
-		r        int
-		k        int
+		nums1 []int
+		nums2 []int
+		k     int
 	}
 	tests := []struct {
 		name string
@@ -81,8 +38,51 @@ func Test_maxPower(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := maxPower(tt.args.stations, tt.args.r, tt.args.k); got != tt.want {
-				t.Errorf("maxPower() = %v, want %v", got, tt.want)
+			if got := minOperations(tt.args.nums1, tt.args.nums2, tt.args.k); got != tt.want {
+				t.Errorf("minOperations() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_maxScore(t *testing.T) {
+	type args struct {
+		nums1 []int
+		nums2 []int
+		k     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxScore(tt.args.nums1, tt.args.nums2, tt.args.k); got != tt.want {
+				t.Errorf("maxScore() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isReachable(t *testing.T) {
+	type args struct {
+		targetX int
+		targetY int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isReachable(tt.args.targetX, tt.args.targetY); got != tt.want {
+				t.Errorf("isReachable() = %v, want %v", got, tt.want)
 			}
 		})
 	}
