698ff6c9f5e32744301905d67da32397c26538b2
zwGithubStation <zwp_sz_pro@163.com>
2023-01-24 19:10:42 +0800

    D 96 3

--- weekCamp/D_96/code/code.go
+++ weekCamp/D_96/code/code.go
@@ -159,6 +159,14 @@ func max(a, b int) int {
 提示：
 1 <= targetX, targetY <= 109
 */
-func isReachable(targetX int, targetY int) bool {
-	return false
+func isReachable(targetX, targetY int) bool {
+	g := gcd(targetX, targetY)
+	return g&(g-1) == 0
+}
+
+func gcd(a, b int) int {
+	for a != 0 {
+		a, b = b%a, a
+	}
+	return b
 }
