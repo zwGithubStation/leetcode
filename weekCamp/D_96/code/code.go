bfe678c21246a84fde9db62fe524772db94dcc80
zwGithubStation <zwp_sz_pro@163.com>
2023-01-24 19:08:59 +0800

    D 96 1

--- weekCamp/D_96/code/code.go
+++ weekCamp/D_96/code/code.go
@@ -47,8 +47,27 @@ n == nums1.length == nums2.length
 0 <= nums1[i], nums2[j] <= 109
 0 <= k <= 105
 */
-func minOperations(nums1 []int, nums2 []int, k int) int64 {
-	return 1
+
+func minOperations(nums1, nums2 []int, k int) (ans int64) {
+	sum := 0
+	for i, x := range nums1 {
+		x -= nums2[i]
+		if k > 0 {
+			if x%k != 0 {
+				return -1
+			}
+			sum += x / k
+			if x > 0 {
+				ans += int64(x / k)
+			}
+		} else if x != 0 {
+			return -1
+		}
+	}
+	if sum != 0 {
+		return -1
+	}
+	return
 }
 
 /*
