ef78dc504488ca6546149673987b1abf1a6622c0
zwGithubStation <zwp_sz_pro@163.com>
2023-01-24 19:09:53 +0800

    D 96 3

--- weekCamp/D_96/code/code.go
+++ weekCamp/D_96/code/code.go
@@ -1,5 +1,10 @@
 package code
 
+import (
+	"container/heap"
+	"sort"
+)
+
 /*
 给你两个整数数组 nums1 和 nums2 ，它们已经按非降序排序，请你返回两个数组的 最小公共整数 。如果两个数组 nums1 和 nums2 没有公共整数，请你返回 -1 。
 如果一个整数在两个数组中都 至少出现一次 ，那么这个整数是数组 nums1 和 nums2 公共 的。
@@ -98,8 +103,41 @@ n == nums1.length == nums2.length
 0 <= nums1[i], nums2[j] <= 105
 1 <= k <= n
 */
-func maxScore(nums1 []int, nums2 []int, k int) int64 {
-	return 1
+func maxScore(nums1, nums2 []int, k int) int64 {
+	type pair struct{ x, y int }
+	a := make([]pair, len(nums1))
+	sum := 0
+	for i, x := range nums1 {
+		a[i] = pair{x, nums2[i]}
+	}
+	sort.Slice(a, func(i, j int) bool { return a[i].y > a[j].y })
+
+	h := hp{nums2[:k]} // 复用内存
+	for i, p := range a[:k] {
+		sum += p.x
+		h.IntSlice[i] = p.x
+	}
+	ans := sum * a[k-1].y
+	heap.Init(&h)
+	for _, p := range a[k:] {
+		if p.x > h.IntSlice[0] {
+			sum += p.x - h.replace(p.x)
+			ans = max(ans, sum*p.y)
+		}
+	}
+	return int64(ans)
+}
+
+type hp struct{ sort.IntSlice }
+
+func (hp) Pop() (_ interface{}) { return }
+func (hp) Push(interface{})     {}
+func (h hp) replace(v int) int  { top := h.IntSlice[0]; h.IntSlice[0] = v; heap.Fix(&h, 0); return top }
+func max(a, b int) int {
+	if b > a {
+		return b
+	}
+	return a
 }
 
 /*
