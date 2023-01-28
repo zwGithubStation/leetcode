d309cd736ac3b6c43d50b1fbd8414303df671b1a
zwGithubStation <zwp_sz_pro@163.com>
2023-01-28 22:32:36 +0800

    329 1 3

--- weekCamp/329/code/code.go
+++ weekCamp/329/code/code.go
@@ -83,23 +83,31 @@ func sortTheStudents2(score [][]int, k int) [][]int {
 }
 
 /*
-func sortTheStudents(score [][]int, k int) [][]int {
-    kth := make([]int, len(score))
-    mm := make(map[int]int)
-    for i, s := range score {
-        kth[i] = s[k]
-        mm[s[k]] = i
-    }
-    sort.Ints(kth)
-    sco := [][]int{}
-    for i := len(kth)-1; i >= 0; i-- {
-        sco = append(sco, score[mm[kth[i]]])
-    }
-    return sco
-}
+给你两个下标从 0 开始的 二元 字符串 s 和 target ，两个字符串的长度均为 n 。你可以对 s 执行下述操作 任意 次：
+选择两个 不同 的下标 i 和 j ，其中 0 <= i, j < n 。
+同时，将 s[i] 替换为 (s[i] OR s[j]) ，s[j] 替换为 (s[i] XOR s[j]) 。
+例如，如果 s = "0110" ，你可以选择 i = 0 和 j = 2，然后同时将 s[0] 替换为 (s[0] OR s[2] = 0 OR 1 = 1)，
+并将 s[2] 替换为 (s[0] XOR s[2] = 0 XOR 1 = 1)，最终得到 s = "1110" 。
+如果可以使 s 等于 target ，返回 true ，否则，返回 false 。
+示例 1：
+输入：s = "1010", target = "0110"
+输出：true
+解释：可以执行下述操作：
+- 选择 i = 2 和 j = 0 ，得到 s = "0010".
+- 选择 i = 2 和 j = 1 ，得到 s = "0110".
+可以使 s 等于 target ，返回 true 。
+示例 2：
+输入：s = "11", target = "00"
+输出：false
+解释：执行任意次操作都无法使 s 等于 target 。
+提示：
+n == s.length == target.length
+2 <= n <= 10e5
+s 和 target 仅由数字 0 和 1 组成
 */
 func makeStringsEqual(s string, target string) bool {
-
+	//pass for now
+	return false
 }
 
 func minCost(nums []int, k int) int {
