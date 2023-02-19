510d296cb139d3aa9640184c97a2f20213a7dfd2
zwGithubStation <zwp_sz_pro@163.com>
2023-02-19 16:04:36 +0800

    D98

--- weekCamp/D_98/code/code.go
+++ weekCamp/D_98/code/code.go
@@ -61,9 +61,9 @@ func minimizeSum(nums []int) int {
 }
 
 /*
-你一个下标从 0 开始的整数数组 nums 。
-如果存在一些整数满足 0 <= index1 < index2 < ... < indexk < nums.length ，得到 nums[index1] | nums[index2] | ... | nums[indexk] = x ，那么我们说 x 是 可表达的 。换言之，如果一个整数能由 nums 的某个子序列的或运算得到，那么它就是可表达的。
-请你返回 nums 不可表达的 最小非零整数 。
+你一个下标从 0 开始的整数数组 nums 。
+如果存在一些整数满足 0 <= index1 < index2 < ... < indexk < nums.length ，得到 nums[index1] | nums[index2] | ... | nums[indexk] = x ，那么我们说 x 是 可表达的 。换言之，如果一个整数能由 nums 的某个子序列的或运算得到，那么它就是可表达的。
+请你返回 nums 不可表达的 最小非零整数 。
 示例 1：
 输入：nums = [2,1]
 输出：4
@@ -82,27 +82,30 @@ func minImpossibleOR(nums []int) int {
 }
 
 /*
-给你一个下标从 0 开始的 m x n 二进制 矩阵 grid 。你可以从一个格子 (row, col) 移动到格子 (row + 1, col) 或者 (row, col + 1) ，
-前提是前往的格子值为 1 。如果从 (0, 0) 到 (m - 1, n - 1) 没有任何路径，我们称该矩阵是 不连通 的。
-你可以翻转 最多一个 格子的值（也可以不翻转）。你 不能翻转 格子 (0, 0) 和 (m - 1, n - 1) 。
-如果可以使矩阵不连通，请你返回 true ，否则返回 false 。
-注意 ，翻转一个格子的值，可以使它的值从 0 变 1 ，或从 1 变 0 。
+给你两个下标从 0 开始的数组 nums1 和 nums2 ，和一个二维数组 queries 表示一些操作。总共有 3 种类型的操作：
+操作类型 1 为 queries[i] = [1, l, r] 。你需要将 nums1 从下标 l 到下标 r 的所有 0 反转成 1 或将 1 反转成 0 。l 和 r 下标都从 0 开始。
+操作类型 2 为 queries[i] = [2, p, 0] 。对于 0 <= i < n 中的所有下标，令 nums2[i] = nums2[i] + nums1[i] * p 。
+操作类型 3 为 queries[i] = [3, 0, 0] 。求 nums2 中所有元素的和。
+请你返回一个数组，包含所有第三种操作类型的答案。
 示例 1：
-输入：grid = [[1,1,1],[1,0,0],[1,1,1]]
-输出：true
-解释：按照上图所示我们翻转蓝色格子里的值，翻转后从 (0, 0) 到 (2, 2) 没有路径。
+输入：nums1 = [1,0,1], nums2 = [0,0,0], queries = [[1,1,1],[2,1,0],[3,0,0]]
+输出：[3]
+解释：第一个操作后 nums1 变为 [1,1,1] 。第二个操作后，nums2 变成 [1,1,1] ，所以第三个操作的答案为 3 。所以返回 [3] 。
 示例 2：
-输入：grid = [[1,1,1],[1,0,1],[1,1,1]]
-输出：false
-解释：无法翻转至多一个格子，使 (0, 0) 到 (2, 2) 没有路径。
+输入：nums1 = [1], nums2 = [5], queries = [[2,0,0],[3,0,0]]
+输出：[5]
+解释：第一个操作后，nums2 保持不变为 [5] ，所以第二个操作的答案是 5 。所以返回 [5] 。
 提示：
-m == grid.length
-n == grid[i].length
-1 <= m, n <= 1000
-1 <= m * n <= 10e5
-grid[0][0] == grid[m - 1][n - 1] == 1
+1 <= nums1.length,nums2.length <= 10e5
+nums1.length = nums2.length
+1 <= queries.length <= 10e5
+queries[i].length = 3
+0 <= l <= r <= nums1.length - 1
+0 <= p <= 10e6
+0 <= nums1[i] <= 1
+0 <= nums2[i] <= 10e9
 */
 
-func isPossibleToCutPath(grid [][]int) bool {
-	return false
+func handleQuery(nums1 []int, nums2 []int, queries [][]int) []int64 {
+	return []int64{}
 }
