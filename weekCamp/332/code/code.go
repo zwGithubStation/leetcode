eda18e68b735c23c5797b15ef1701175c0d27c87
zwGithubStation <zwp_sz_pro@163.com>
2023-02-19 15:43:10 +0800

    332

--- weekCamp/332/code/code.go
+++ weekCamp/332/code/code.go
@@ -47,33 +47,28 @@ func findTheArrayConcVal(nums []int) int64 {
 }
 
 /*
-给你一个下标从 0 开始的字符串数组 words 以及一个二维整数数组 queries 。
-每个查询 queries[i] = [li, ri] 会要求我们统计在 words 中下标在 li 到 ri 范围内（包含 这两个值）并且以元音开头和结尾的字符串的数目。
-返回一个整数数组，其中数组的第 i 个元素对应第 i 个查询的答案。
-注意：元音字母是 'a'、'e'、'i'、'o' 和 'u' 。
+给你一个下标从 0 开始、长度为 n 的整数数组  nums  ，和两个整数  lower 和  upper ，返回 公平数对的数目 。
+如果  (i, j)  数对满足以下情况，则认为它是一个 公平数对  ：
+0 <= i < j < n，且
+lower <= nums[i] + nums[j] <= upper
+
 示例 1：
-输入：words = ["aba","bcb","ece","aa","e"], queries = [[0,2],[1,4],[1,1]]
-输出：[2,3,0]
-解释：以元音开头和结尾的字符串是 "aba"、"ece"、"aa" 和 "e" 。
-查询 [0,2] 结果为 2（字符串 "aba" 和 "ece"）。
-查询 [1,4] 结果为 3（字符串 "ece"、"aa"、"e"）。
-查询 [1,1] 结果为 0 。
-返回结果 [2,3,0] 。
+输入：nums = [0,1,7,4,4,5], lower = 3, upper = 6
+输出：6
+解释：共计 6 个公平数对：(0,3)、(0,4)、(0,5)、(1,3)、(1,4) 和 (1,5) 。
 示例 2：
-输入：words = ["a","e","i"], queries = [[0,2],[0,1],[2,2]]
-输出：[3,2,1]
-解释：每个字符串都满足这一条件，所以返回 [3,2,1] 。
+输入：nums = [1,7,9,2,5], lower = 11, upper = 11
+输出：1
+解释：只有单个公平数对：(2,3) 。
 提示：
-1 <= words.length <= 10e5
-1 <= words[i].length <= 40
-words[i] 仅由小写英文字母组成
-sum(words[i].length) <= 3 * 10e5
-1 <= queries.length <= 10e5
-0 <= queries[j][0] <= queries[j][1] < words.length
+1 <= nums.length <= 10e5
+nums.length == n
+-10e9  <= nums[i] <= 10e9
+-10e9  <= lower <= upper <= 10e9
 */
 
-func vowelStrings(words []string, queries [][]int) []int {
-	return []int{}
+func countFairPairs(nums []int, lower int, upper int) int64 {
+	return 0
 }
 
 /*
