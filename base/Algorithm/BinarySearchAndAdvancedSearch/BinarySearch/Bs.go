fcf62ef5833101f6ef37dc0f006843397b5bd10f
zwGithubStation <zwp_sz_pro@163.com>
2023-02-20 20:06:37 +0800

    binary search

--- base/Algorithm/BinarySearchAndAdvancedSearch/BinarySearch/Bs.go
+++ base/Algorithm/BinarySearchAndAdvancedSearch/BinarySearch/Bs.go
@@ -76,3 +76,34 @@ func lowerBound3(nums []int, target int) int {
 
 //6. <= 转换为 >=
 // <=target的第一个数(升序序列中从后往前遍历的第一个) 等价于 >target的第一个数前面的一个数， 也即lowerBound(target+1) - 1
+
+//例题
+
+//34. https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/
+/*
+给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+如果数组中不存在目标值 target，返回 [-1, -1]。
+你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+示例 1：
+输入：nums = [5,7,7,8,8,10], target = 8
+输出：[3,4]
+示例 2：
+输入：nums = [5,7,7,8,8,10], target = 6
+输出：[-1,-1]
+示例 3：
+输入：nums = [], target = 0
+输出：[-1,-1]
+提示：
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums 是一个非递减数组
+-109 <= target <= 109
+*/
+func searchRange(nums []int, target int) []int {
+	start := lowerBound(nums, target) //调用lowerBound or 调用lowerBound1 or 调用lowerBound2 都可以！
+	if start == len(nums) || nums[start] != target {
+		return []int{-1, -1}
+	}
+	end := lowerBound(nums, target+1) - 1 //调用lowerBound or 调用lowerBound1 or 调用lowerBound2 都可以！
+	return []int{start, end}
+}
