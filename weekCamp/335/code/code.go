af8f7cb3df29e1ca90b008bd85a1aae6b68b0702
zwGithubStation <zwp_sz_pro@163.com>
2023-03-05 20:41:13 +0800

    335 2

--- weekCamp/335/code/code.go
+++ weekCamp/335/code/code.go
@@ -26,19 +26,10 @@ func passThePillow(n int, time int) int {
 
 /*
 给你一棵二叉树的根节点 root 和一个正整数 k 。
-
 树中的 层和 是指 同一层 上节点值的总和。
-
 返回树中第 k 大的层和（不一定不同）。如果树少于 k 层，则返回 -1 。
-
 注意，如果两个节点与根节点的距离相同，则认为它们在同一层。
-
-
-
 示例 1：
-
-
-
 输入：root = [5,8,9,2,1,3,7,4,6], k = 2
 输出：13
 解释：树中每一层的层和分别是：
@@ -48,28 +39,26 @@ func passThePillow(n int, time int) int {
 - Level 4: 4 + 6 = 10
 第 2 大的层和等于 13 。
 示例 2：
-
-
-
 输入：root = [1,2,null,3], k = 1
 输出：3
 解释：最大的层和是 3 。
-
-
 提示：
-
 树中的节点数为 n
-2 <= n <= 105
-1 <= Node.val <= 106
+2 <= n <= 10e5
+1 <= Node.val <= 10e6
 1 <= k <= n
-
-来源：力扣（LeetCode）
-链接：https://leetcode.cn/problems/kth-largest-sum-in-a-binary-tree
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 
-func divisibilityArray(word string, m int) []int {
-	return []int{}
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func kthLargestLevelSum(root *TreeNode, k int) int64 {
+	return 0
 }
 
 /*
