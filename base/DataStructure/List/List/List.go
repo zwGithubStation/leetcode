d38ce68fa510ab2cf36727602aad003b3d684420
zwGithubStation <zwp_sz_pro@163.com>
2023-04-19 18:01:23 +0800

    list

--- base/DataStructure/List/List/List.go
+++ base/DataStructure/List/List/List.go
@@ -1,1 +1,96 @@
 package List
+
+/*
+206 反转链表
+https://leetcode.cn/problems/reverse-linked-list/
+*/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+
+}
+
+/*
+92 反转链表2
+https://leetcode.cn/problems/reverse-linked-list-ii/
+*/
+func reverseBetween(head *ListNode, left int, right int) *ListNode {
+
+}
+
+/*
+25. K 个一组翻转链表
+https://leetcode.cn/problems/reverse-nodes-in-k-group/
+*/
+func reverseKGroup(head *ListNode, k int) *ListNode {
+
+}
+
+/*
+876. 链表的中间结点
+https://leetcode.cn/problems/middle-of-the-linked-list/
+*/
+
+func middleNode(head *ListNode) *ListNode {
+
+}
+
+/*
+141. 环形链表
+https://leetcode.cn/problems/linked-list-cycle/
+*/
+func hasCycle(head *ListNode) bool {
+
+}
+
+/*
+142. 环形链表 II
+https://leetcode.cn/problems/linked-list-cycle-ii/
+*/
+func detectCycle(head *ListNode) *ListNode {
+
+}
+
+/*
+143. 重排链表
+https://leetcode.cn/problems/reorder-list/
+*/
+func reorderList(head *ListNode) {
+
+}
+
+/*
+237. 删除链表中的节点
+https://leetcode.cn/problems/delete-node-in-a-linked-list/
+*/
+func deleteNode(node *ListNode) {
+
+}
+
+/*
+19. 删除链表的倒数第 N 个结点
+https://leetcode.cn/problems/remove-nth-node-from-end-of-list/
+*/
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+
+}
+
+/*
+83. 删除排序链表中的重复元素
+https://leetcode.cn/problems/remove-duplicates-from-sorted-list/
+*/
+func deleteDuplicates(head *ListNode) *ListNode {
+
+}
+
+/*
+82. 删除排序链表中的重复元素 II
+https://leetcode.cn/problems/remove-duplicates-from-sorted-list-ii/
+*/
+func deleteDuplicates2(head *ListNode) *ListNode {
+
+}
